b3475c827fd61828cf51431a2b898a0d3cd970a5
Tilo Spannagel <development@tilosp.de>
2023-10-27 15:43:25 +0000

    Apply workaround from fork
    See: https://github.com/mesalock-linux/yasna.rs-sgx/commit/a7b9bf193c7335be62151c714eb3ca9cf91ab269#diff-57cb85c1fc6839522f920168d8a7527a44bc8f9ea98f962edf17832a8357e6ec

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -179,7 +179,8 @@ pub fn try_construct_der_seq<F, E>(callback: F) -> Result<Vec<u8> , E>
 /// ```
 #[derive(Debug)]
 pub struct DERWriter<'a> {
-    buf: &'a mut Vec<u8>,
+    /// Make this pub for Rust-SGX
+    pub buf: &'a mut Vec<u8>,
     implicit_tag: Option<Tag>,
 }
 
@@ -1212,7 +1213,8 @@ impl<'a> DERWriter<'a> {
 /// ```
 #[derive(Debug)]
 pub struct DERWriterSeq<'a> {
-    buf: &'a mut Vec<u8>,
+    /// Make this pub for Rust-SGX
+    pub buf: &'a mut Vec<u8>,
 }
 
 impl<'a> DERWriterSeq<'a> {
